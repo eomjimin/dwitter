247936d7106617486caf544107cf4f9708f1b7e4
eomjimin <jimini0920@naver.com>
2023-11-17 10:29:24 +0900

    Mongoose 적용

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import tweetRouter from "./router/tweets.js";
 import authRouter from "./router/auth.js";
 import {config} from './config.js';
 import { initSocket } from "./connection/socket.js";
-import { db } from "./db/database.js";
+import { connectDB } from "./db/database.js";
 
 console.log(process.env.JWT_SECRET);
 const app = express();
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
     res.sendStatus(404);
 });
 
-db.getConnection().then(connection => console.log(connection));
-
-const server = app.listen(config.host.port);
-initSocket(server);
+connectDB().then(() => {
+    const server = app.listen(config.host.port);
+    initSocket(server);
+}).catch(console.error);
--- data/auth.js
+++ data/auth.js
@@ -1,71 +1,4 @@
-/* 
-[/router/auth.js] -> 과제부분
 
-    회원가입
-    router.post("/signup", ...) -> post로 json 으로 create 되었는지 만들어보기
-    
-    로그인
-    router.post("/login", ...) -> username/password를 post로 보내 그런 사람이 있는지를 확인 
-    -> JWT를 만들어내고, 만료 날짜가 주어짐
-
-    JWT 확인
-    router.get("/me", ...) -> 모든 페이지를 돌 때 먼저 한번씩 실행, //남겨놓기
-
-*/
-// import bcrypt from "bcrypt";
-// import jwt from "jsonwebtoken";
-
-// let users = [
-
-//     {
-//         id: "1",
-//         username: "apple",
-//         password: "$2b$10$6NVVL4gEtPh684Ncn2sCRe/LPe0u4kRkhBYSoiLx4bTGW5gwQ58Dy",   //abcd1234
-//         name: "김사과",
-//         email: "apple@apple.com",
-//         url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrYEhHx-OXQF1NqVRXPL8R50ggKje3hQTvIA&usqp=CAU"
-//     }
-
-// ];
-
-// export async function create(id, username, password, name, email, url) {
-//     const hpw = bcrypt.hashSync(password, 10);
-//     const user = {
-//         id,
-//         username,
-//         password: hpw,
-//         name,
-//         email,
-//         url
-//     };
-//     users = [user, ...users];
-//     return users;
-// }
-
-// export async function signin(username, password) {
-//     const user = users.filter((user) => user.username === username);
-//     const expiresIn = 1200;
-//     let Element;
-//     if (user) {
-//         user.forEach(element => {
-//             const result = bcrypt.compareSync(password, element.password);
-//             if (result) {
-//                 const token = jwt.sign(
-//                     {
-//                         username,
-//                         isAdmin: false
-//                     },
-//                     password,
-//                     { expiresIn }
-//                 );
-//                 Element = element
-//                 console.log(Element);
-//             }
-//         });
-//         Element.expiresIn = expiresIn;
-//         return Element;
-//     }
-// }
 /*
     회원가입
     router.post("/signup", ...) -> post로 json 으로 create 되었는지 만들어보기
@@ -78,23 +11,23 @@
     router.get("/me", ...) -> 모든 페이지를 돌 때 먼저 한번씩 실행, //남겨놓기
 
 */ 
-import bcrypt, { hashSync } from "bcrypt";
-
-import { db } from "../db/database.js";
-
-
-
-export async function findByUsername(username){
-    return db.execute('SELECT * FROM users WHERE username = ?', [username]).then((result) => result[0][0]);
+import Mongoose from "mongoose";
+import { useVirtualId } from "../db/database.js";
+const userSchema = new Mongoose.Schema({
+    username: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    url: String
+});
+useVirtualId(userSchema);
+const User = Mongoose.model('User', userSchema);
+export async function findByUsername(username) {
+    return User.findOne({ username });
 }
-
-export async function findById(id){
-    // return users.find((user) => user.id === id);
-    return db.execute('SELECT * FROM users WHERE id = ?', [id]).then((result) => result[0][0]);
-
+export async function findById(id) {
+    return User.findById(id);
 }
-
-export async function createUser(user){
-    const {username, password, name, email, url} = user;
-    return db.execute('INSERT INTO users (username, password, name, email, url) VALUES (?, ?, ?, ?, ?)', [username, password, name, email, url]).then((result) => result[0].insertId);
+export async function createUser(user) {
+    return new User(user).save().then((data) => data.id);
 }
